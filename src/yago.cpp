6b46d5abe084f744b334b92464727b377365cd76
Florian Walch <florian@fwalch.com>
2013-02-04 13:49:22 +0100

    Adapted Yago query for string vector
    Conflicts:
    	src/yago.cpp

--- src/yago.cpp
+++ src/yago.cpp
@@ -1,18 +1,15 @@
 #include "yago.h"
 #include <set>
 #include <iostream>
+#include <cstring>
 
 size_t Yago::queryWebsitesOfAllWomen() {
-  std::set<str::string> women;
-  str::string hasGender = facts.predicate_dict.get_string("hasGender");
-  str::string female = facts.object_dict.get_string("female");
-  str::string hasWebsite = facts.predicate_dict.get_string("hasWebsite");
+  std::set<std::string> women;
   size_t websites = 0;
 
   // Get all women
-  size_t i = 0;
-  for (auto predicate : facts.predicate) {
-    if (predicate == hasGender && facts.object[i] == female) {
+  for (size_t i = 0; i < facts.count(); i++) {
+    if (strcmp(facts.predicate[i], "hasGender") == 0 && strcmp(facts.object[i], "female") == 0) {
       // Store to find website
       women.insert(facts.subject[i]);
     }
@@ -21,9 +18,8 @@ size_t Yago::queryWebsitesOfAllWomen() {
 
   // Get all websites
   auto endIterator = women.end();
-  i = 0;
-  for (auto predicate : facts.predicate) {
-    if (predicate == hasWebsite && women.find(facts.subject[i]) != endIterator) {
+  for (size_t i = 0; i < facts.count(); i++) {
+    if (strcmp(facts.predicate[i], "hasWebsite") == 0 && women.find(facts.subject[i]) != endIterator) {
       websites++;
     }
     i++;
